3c9a0eaf505465f37dda0d6773b27cf1c3768614
wj-mi <997457785@qq.com>
2018-09-13 22:51:28 +0800

    add log and  发牌

--- room.py
+++ room.py
@@ -8,12 +8,36 @@ from DZ.Cards_gen import card_gen
 from DZ.DZ_roboot import Users
 
 
-def game_start():
+def game_start(pokers):
     """游戏开始，发牌，确认地主"""
-    pass
+    index = 0
+    for user in Users:
+        user.cards = pokers[index: index+18]
+        index = index+18
+        logging.info("{}'s cards: {}".format(user.name, user.cards))
+
+
+def log_config():
+    logging.basicConfig(level=logging.DEBUG,
+
+                        format='%(levelname)s %(asctime)s %(message)s',
+
+                        datefmt='%a, %d %b %Y %H:%M:%S',
+
+                        filename='DZ.log',
+
+                        filemode='w')
 
 
 def main():
+    log_config()
+    logging.info("New Game start:")
+    pokers, laizi = card_gen()
+    logging.info("cards: {}".format(pokers))
+    logging.info("laizi card: {}".format(laizi))
     # 发牌
-    game_start()
+    game_start(pokers)
 
+
+if __name__ == '__main__':
+    main()
